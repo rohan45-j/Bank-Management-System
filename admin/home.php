1f190136debae5cb8b60b0301fc614dfdee5d4a1
rohan45-j <72876459+rohan45-j@users.noreply.github.com>
2023-04-05 01:46:02 +0600

    Add files via upload

---
+++ admin/home.php
@@ -0,0 +1,109 @@
+
+<h1 class="text-dark">Welcome to <?php echo $_settings->info('name') ?></h1>
+<?php
+?>
+
+<hr>
+<div class="container-fluid">
+<div class="row">
+          <div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box">
+              <span class="info-box-icon bg-info elevation-1"><i class="fas fa-id-card"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Total Accounts</span>
+                <span class="info-box-box">
+                  <?php 
+                    echo number_format($conn->query("SELECT * FROM accounts")->num_rows);
+                  ?>
+                </span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+          <div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box mb-3">
+              <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-money-bill"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Total Accounts Balance</span>
+                <span class="info-box-number">
+                    <?php echo number_format($conn->query("SELECT sum(balance) as total FROM accounts")->fetch_assoc()['total']); ?>
+                </span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+        </div>
+</div>
+
+
+
+
+<div id="myModal" class="modal">
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <iframe src="https://dashboard.tawk.to/?lang=en#/chat" frameborder="0" height="700" width="100%" style="padding-left: 0;"></iframe>
+  </div>
+
+</div>
+<script>
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+</script>
+<style>
+body {font-family: Arial, Helvetica, sans-serif;}
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  padding-top: 100px; /* Location of the box */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
+
+/* Modal Content */
+.modal-content {
+  background-color: #fefefe;
+  margin: auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+}
+
+/* The Close Button */
+.close {
+  color: #aaaaaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: #000;
+  text-decoration: none;
+  cursor: pointer;
+}
+</style>
